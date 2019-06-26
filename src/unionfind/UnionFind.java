2a749101d88e3b7e1b932c5c1c8e3341236c31a7
mscyan <yanchao00001@hotmail.com>
2019-06-26 23:11:31 +0800

    并查集 需要图相关知识；

---
+++ src/unionfind/UnionFind.java
@@ -0,0 +1,25 @@
+package unionfind;
+
+/**
+ * 并查集
+ */
+public class UnionFind {
+
+    private int node[];
+
+    /**
+     * 找到根节点
+     * @param value
+     */
+    public int unionSearch(int value){
+
+        return 0;
+    }
+
+    /**
+     * 将两个分支连通起来
+     */
+    public void join(){
+
+    }
+}
