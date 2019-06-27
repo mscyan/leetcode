0f4abc345906894dd569d5c2984f24c88ac5357c
yanchao <yanchao@00001@hotmail.com>
2019-06-27 13:33:03 +0800

    路径压缩；

--- src/unionfind/UnionFind.java
+++ src/unionfind/UnionFind.java
@@ -6,9 +6,16 @@ package unionfind;
 public class UnionFind {
 
     private int node[];
+    private int groupCount = 0;
+    private int[] rank;
 
     public UnionFind(int[] arr){
         this.node = arr;
+//        for(int i=0;i<node.length;i++){
+//            node[i] = i;
+//        }
+        groupCount = node.length;
+        this.rank = new int[arr.length];
     }
 
     /**
@@ -20,38 +27,51 @@ public class UnionFind {
             return -1;
         }
         int findCount = 0;
-        int temp = value;
-        if(temp == node[value]){
+        int groupRoot = value;
+        if(groupRoot == node[value]){
             System.out.println("times: " + value + " -> findCount " + findCount);
             return value;
         }
-        while(temp != node[temp]){
-            temp = node[temp];
+        while(groupRoot != node[groupRoot]){
+            groupRoot = node[groupRoot];
             findCount++;
         }
+
+        // 路径压缩算法；
+        int r = value;
+        while(r != groupRoot){
+            int temp = node[r];
+            node[r] = groupRoot;
+            r = temp;
+        }
         System.out.println("times: " + value + " -> findCount " + findCount);
-        return temp;
+        return groupRoot;
     }
 
     /**
      * 将两个分支连通起来
+     * //todo 秩优化，降低合并之后的树的深度；
      */
     public void join(int g1, int g2){
         int root1 = unionSearch(g1);
         int root2 = unionSearch(g2);
         if(root1 != root2){
-            node[root2] = node[root1];
+            node[root2] = root1;
+            groupCount--;
         }
-        //todo 路径压缩算法，降低树的深度；
     }
 
     public static void main(String[] args){
-        int[] arr = {0, 1, 1, 0, 2, 0, 6, 4, 0, 6, 11, 10};
+        //           0  1  2  3  4  5  6  7  8  9  10 11
+        int[] arr = {0, 1, 1, 0, 2, 0, 6, 4, 0, 6, 9, 7};
         UnionFind unionFind = new UnionFind(arr);
-        System.out.println(unionFind.unionSearch(7));
-        System.out.println(unionFind.unionSearch(9));
-        unionFind.join(7,9);
-        System.out.println(unionFind.unionSearch(7));
-        System.out.println(unionFind.unionSearch(9));
+//        System.out.println(unionFind.unionSearch(7));
+//        System.out.println(unionFind.unionSearch(9));
+//        unionFind.join(7,9);
+//        System.out.println(unionFind.unionSearch(7));
+//        System.out.println(unionFind.unionSearch(9));
+
+        System.out.println(unionFind.unionSearch(11));
+        System.out.println(unionFind.unionSearch(11));
     }
 }
